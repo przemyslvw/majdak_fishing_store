7fb07ffb8c0c06bc175df3edadcffcb9128c1d50
przemyslvw <przemyslvw@gmial.com>
2024-05-29 11:39:06 +0200

    init files

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *
+ * @link https://majdak.online
+ *
+ * @package WordPress
+ * @subpackage majdak_fishing_store
+ * @since 1.0.0
+ */
+get_header(); ?>
+<?php 
+
+?>
+<?php get_footer(); ?>
