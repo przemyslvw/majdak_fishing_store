2f36e499e41173a27c1404e1dbd9aab9159c42a0
przemyslvw <przemyslvw@gmial.com>
2024-06-11 09:03:56 +0200

    woocommerce_support

--- functions.php
+++ functions.php
@@ -76,3 +76,9 @@ function create_woocommerce_pages()
 }
 
 add_action('after_switch_theme', 'create_woocommerce_pages');
+
+function mytheme_add_woocommerce_support()
+{
+    add_theme_support('woocommerce');
+}
+add_action('after_setup_theme', 'mytheme_add_woocommerce_support');
